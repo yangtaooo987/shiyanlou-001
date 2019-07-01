f79e4b46726ec909287f0451c6f3cf69d2e8c3cf
yangtaooo987 <942071771@qq.com>
2019-07-01 12:01:07 +0800

    3-1

---
+++ c/3-1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int a,b,c;
+    printf("Please enter a value:");
+    scanf("%d",&a);
+    printf("\n");
+    printf("Please enter b value:");
+    scanf("%d",&b);
+    c = a+b;
+    printf("\n%d\n",c);
+    return 0;
+}
